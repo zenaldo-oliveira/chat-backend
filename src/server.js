81fec2d16b9fbf8456331c07034a683228b8577f
Zenaldo Oliveira <zenaldo.221@hotmail.com>
2025-04-11 00:53:51 -0400

    chat client to use Socket.IO instead of native WebSocket

--- src/server.js
+++ src/server.js
@@ -1,25 +1,45 @@
-const { WebSocketServer } = require("ws");
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
 const dotenv = require("dotenv");
 
 dotenv.config();
 
 const PORT = process.env.PORT || 8080;
-const wss = new WebSocketServer({ port: PORT });
 
-wss.on("connection", (ws) => {
-  console.log("🟢 New client connected");
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*", // Permite acesso de qualquer origem
+  },
+});
 
-  ws.on("error", console.error);
+io.on("connection", (socket) => {
+  console.log("🟢 New client connected..", socket.id);
 
-  ws.on("message", (data) => {
-    const message = data.toString();
+  socket.on("login", ({ userName }) => {
+    const systemMessage = {
+      system: true,
+      content: `👋 ${userName} entrou no chat.`,
+    };
 
-    wss.clients.forEach((client) => {
-      if (client.readyState === ws.OPEN) {
-        client.send(message);
-      }
-    });
+    // Envia para todos os conectados
+    io.emit("chatMessage", systemMessage);
+  });
+
+  socket.on("message", (data) => {
+    // data já pode ser um objeto com userName, content, etc.
+    io.emit("chatMessage", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("🔴 Usuário desconectado:", socket.id);
   });
 });
 
-console.log(`✅ WebSocket server is up and running on ws://localhost:${PORT}`);
+server.listen(PORT, () => {
+  console.log(
+    `✅ Socket.IO server is up and running on http://localhost:${PORT}`
+  );
+});
